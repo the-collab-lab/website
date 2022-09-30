3cbf6b5afc2a890142e48c38031cd01b986c80b0
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-09-30 15:46:35 -0700

    chore: format eslintconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,40 @@
 module.exports = {
-  root: true,
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-  plugins: ['@typescript-eslint', 'import', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:astro/recommended',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [
-    {
-      // Define the configuration for `.astro` file.
-      files: ['*.astro'],
-      // Allows Astro components to be parsed.
-      parser: 'astro-eslint-parser',
-      // Parse the script in `.astro` as TypeScript by adding the following configuration.
-      // It's the setting you need when using TypeScript.
-      parserOptions: {
-        extraFileExtensions: ['.astro'],
-        parser: '@typescript-eslint/parser',
-      },
-    },
-  ],
+	root: true,
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+	env: {
+		browser: true,
+		node: true,
+	},
+	plugins: ['@typescript-eslint', 'import', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:astro/recommended',
+		'plugin:prettier/recommended',
+	],
+	overrides: [
+		{
+			// Define the configuration for `.astro` file.
+			files: ['*.astro'],
+			// Allow Astro components to be parsed.
+			parser: 'astro-eslint-parser',
+			// Parse the script in `.astro` as TypeScript.
+			parserOptions: {
+				extraFileExtensions: ['.astro'],
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
 };
