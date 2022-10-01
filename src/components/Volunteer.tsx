b0accc90ce2f546432bf6761b5857b8d11d8d237
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-10-01 10:58:25 -0700

    feat(volunteer): render collabie images

--- src/components/Volunteer.tsx
+++ src/components/Volunteer.tsx
@@ -6,18 +6,34 @@ export interface VolunteerProps {
 
 const socialSiteNames = ['gitHub', 'linkedIn', 'twitter'] as const;
 
+function correctPhotoPath(path: string | null) {
+	return path?.replace(/^\/assets/, '');
+}
+
+const avatarPlaceholderPath = '/img/members/generic-avatar.png';
+
 export function Volunteer({ collabie }: VolunteerProps) {
-	const { fullName, roles } = collabie;
+	const { fullName, pathToPhoto, roles } = collabie;
 	return (
-		<>
-			<h2>{fullName}</h2>
+		<div class="volunteer__grid-item">
+			<figure className="volunteer">
+				<img
+					alt=""
+					class="volunteer__photo"
+					loading="lazy"
+					height="300"
+					src={correctPhotoPath(pathToPhoto) || avatarPlaceholderPath}
+					width="300"
+				/>
+			</figure>
+			<b>{fullName}</b>
 			<ul class="volunteer__roles">
 				{roles.map((role) => (
 					<li class="volunteer__roles-item">{role}</li>
 				))}
 			</ul>
-			<ul>{renderSocials(collabie)}</ul>
-		</>
+			<ul className="volunteer__socials">{renderSocials(collabie)}</ul>
+		</div>
 	);
 }
 
@@ -28,7 +44,7 @@ function renderSocials(collabie: CollabieData) {
 		if (!siteUrl || siteUrl.length === 0) return null;
 		const formattedSiteName = site.charAt(0).toUpperCase() + site.slice(1);
 		return (
-			<li>
+			<li className="volunteer__social-item">
 				<a href={siteUrl}>
 					Connect with {collabie.firstName} on {formattedSiteName}!
 				</a>
