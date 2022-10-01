5acc47e26284168ad5d070d2e8e0de2a7763ea02
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-09-30 18:33:08 -0700

    feat: create DeveloperTeam component

--- src/components/DeveloperTeam.tsx
+++ src/components/DeveloperTeam.tsx
@@ -5,23 +5,43 @@ function correctPhotoPath(path: string) {
 	return path.replace(/^\/assets/, '');
 }
 
-export default function DeveloperTeam({
-	team,
-}: {
-	team: {
-		anchor: string;
-		calculatedDate: string;
-		developers: {
-			fullName: string;
-			firstName: string;
-			pathToPhoto: string;
-			gitHubUrl: string;
-			linkedInUrl: string;
-			twitterUrl: string;
-		}[];
-		displayName: string;
-	};
-}) {
+interface Developer {
+	fullName: string;
+	firstName: string;
+	pathToPhoto: string;
+	gitHubUrl?: string;
+	linkedInUrl?: string;
+	twitterUrl?: string;
+}
+
+export interface DeveloperTeamT {
+	anchor: string;
+	calculatedDate: string;
+	developers: Developer[];
+	displayName: string;
+}
+
+function renderSocials(developer: Developer) {
+	socials.map((site) => {
+		const siteUrl = developer[`${site}Url` as const];
+
+		if (!siteUrl) return null;
+
+		return (
+			<a href={siteUrl} className="social-media-logo">
+				<img
+					alt={site}
+					height="20"
+					loading="lazy"
+					src={`/img/icons/${site.toLowerCase()}.png`}
+					width="20"
+				/>
+			</a>
+		);
+	});
+}
+
+export function DeveloperTeam({ team }: { team: DeveloperTeamT }) {
 	return (
 		<>
 			<h3 id={team.anchor}>
@@ -32,27 +52,15 @@ export default function DeveloperTeam({
 					<li class="member-container">
 						<img
 							alt={developer.fullName}
+							loading="lazy"
 							height="200"
 							src={correctPhotoPath(developer.pathToPhoto)}
 							width="200"
 						/>
-						<p>{developer.firstName}</p>
-						{socials.map((site) => {
-							const siteUrl = developer[`${site}Url` as const];
-
-							if (!siteUrl) return null;
-
-							return (
-								<a href={siteUrl}>
-									<img
-										alt={site}
-										height="20"
-										src={`/img/icons/${site.toLowerCase()}.png`}
-										width="20"
-									/>
-								</a>
-							);
-						})}
+						<div class="member-caption">
+							<p>{developer.firstName}</p>
+							{renderSocials(developer)}
+						</div>
 					</li>
 				))}
 			</ul>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Card, type Props as CardProps } from './Card.astro';
 export { default as Navigation } from './Navigation.astro';
 export * from './Volunteer';
+export * from './DeveloperTeam';
