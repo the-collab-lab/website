95db1b51f12651ae274c843c53b2c7e3be3fc891
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-10-01 22:03:43 -0700

    feat: process Stripe API data

---
+++ src/data/stripe.ts
@@ -0,0 +1,71 @@
+import Stripe from 'stripe';
+const { STRIPE_PK, STRIPE_SK } = import.meta.env;
+
+interface FormattedPrice extends Stripe.Price {
+	unit_amount: number;
+}
+
+interface FormattedProduct extends Stripe.Product {
+	default_price: FormattedPrice;
+	price: {
+		id: string;
+		formatted_amount: string;
+	};
+}
+
+export const getDonationOptions = async () => {
+	try {
+		const stripe = new Stripe(STRIPE_SK, {
+			apiVersion: '2022-08-01',
+		});
+		const { data: products = [] } = await stripe.products.list({
+			expand: ['data.default_price'],
+		});
+
+		const _options = products.map((option) => {
+			const default_price = option.default_price as FormattedPrice;
+			(option as unknown as FormattedProduct).price = {
+				id: default_price?.id,
+				formatted_amount: String(default_price.unit_amount / 100),
+			};
+
+			return option as unknown as FormattedProduct;
+		});
+
+		const options = _options.sort(
+			(a, b) => a.default_price.unit_amount - b.default_price.unit_amount,
+		);
+
+		return {
+			enabled: options?.length > 0 && STRIPE_PK !== undefined,
+			options,
+		};
+	} catch (err) {
+		return {
+			enabled: false,
+			options: [
+				{
+					price: {
+						id: '000',
+						formatted_amount: '5',
+					},
+					name: 'Yaaaay',
+				},
+				{
+					price: {
+						id: '001',
+						formatted_amount: '10',
+					},
+					name: 'Booster',
+				},
+				{
+					price: {
+						id: '002',
+						formatted_amount: '15',
+						name: 'Amplifier',
+					},
+				},
+			],
+		};
+	}
+};
