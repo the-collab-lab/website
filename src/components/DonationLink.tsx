3088cc8b6e5e4c2ea5cf357665df626898a8b343
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-10-05 21:39:15 -0700

    feat: render donation links directly in footer

--- src/components/DonationLink.tsx
+++
@@ -1,9 +0,0 @@
-import type { DonationOption } from '../data/stripe';
-
-export function DonationLink({ option }: { option: DonationOption }) {
-	return (
-		<a href={option.url} className="donation-button">
-			{option.name}
-		</a>
-	);
-}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,5 @@ export { default as Navigation } from './Navigation.astro';
 // @ts-expect-error TypeScript doesn't like the .astro file extension
 export { default as GetInTouch } from './GetInTouch.astro';
 export * from './DeveloperTeam';
-export * from './DonationLink';
+export * from './DonationButton';
 export * from './Volunteer';
