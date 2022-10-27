3091a5a21fd873f264bdc324bfd41426b69cea6f
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-10-27 15:16:46 -0700

    feat: create footer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 // @ts-expect-error TypeScript doesn't like the .astro file extension
 export { default as GetInTouch } from './GetInTouch.astro';
 // @ts-expect-error TypeScript doesn't like the .astro file extension
+export { default as Footer } from './Footer.astro';
+// @ts-expect-error TypeScript doesn't like the .astro file extension
 export { default as Header } from './Header.astro';
 // @ts-expect-error TypeScript doesn't like the .astro file extension
 export { default as Navigation } from './Navigation.astro';
