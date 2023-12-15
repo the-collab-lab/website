2c160183fe8144f686d9c6bc3ad416e023afa00d
EJ Mason <13525251+mxmason@users.noreply.github.com>
2023-12-14 18:47:37 -0800

    feat: add sitemap plugun to astro config

--- astro.config.js
+++ astro.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
 	build: {
 		inlineStylesheets: 'auto',
 	},
-	integrations: [preact()],
+	integrations: [preact(), sitemap()],
 	prefetch: {
 		defaultStrategy: 'hover',
 		prefetchAll: true,
